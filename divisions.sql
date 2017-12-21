fa5a04243270bc2fc20b333f543dd2df5788dfad
Abu Bakar Siddique <absiddique.live@gmail.com>
2017-12-21 12:39:20 +0600

    Mymensingh division added

--- divisions.sql
+++ divisions.sql
@@ -38,4 +38,5 @@ INSERT INTO `divisions` (`id`, `name`, `bn_name`) VALUES
 (4, 'Khulna', 'খুলনা'),
 (5, 'Rajshahi', 'রাজশাহী'),
 (6, 'Rangpur', 'রংপুর'),
-(7, 'Sylhet', 'সিলেট');
+(7, 'Sylhet', 'সিলেট'),
+(8, 'Mymensingh', 'ময়মনসিংহ');
